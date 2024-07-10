472b113ac5d4759ac41e764d549e8e138ca9ca90
Anikcse18 <istiakjavedanik@gmail.com>
2024-07-10 12:04:41 +0600

    dada come from api

--- js/ai.js
+++ js/ai.js
@@ -79,8 +79,89 @@ const showdetails = async (ai_id) => {
   const ai_features = data.data;
   showmodals_display(ai_features);
 };
+
 //modals box
 const showmodals_display = (ai_features) => {
+  const ai_modal_box = document.getElementById("ai_modal_box");
+  ai_modal_box.innerHTML = `
+   <form method="dialog">
+                        <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 bg-red-500">✕</button>
+                    </form>
+                    <!-- div design -->
+                    <div class="flex flex-col md:flex-row gap-3 p-4 md:p-6 ">
+                        <!-- products details div -->
+                        <div class="card card-compact bg-red-100 border-red-400 border-[1px]  shadow-xl">
+                            <div class="p-4">
+                                <h1 class="text-lg font-semibold">${
+                                  ai_features.description
+                                }
+                            </div>
+                            <div class="card-body">
+                                <div class="flex flex-col md:flex-row mx-auto gap-3">
+                                    <div class="p-5 bg-white w-1/3 rounded-md">
+                                        <h1 class="text-sm font-semibold text-center text-green-500">Free
+                                            of <br> cost / Basic</h1>
+
+                                    </div>
+                                    <div class="p-5 bg-white w-1/3 rounded-md">
+                                        <h1 class="text-sm font-semibold text-center text-red-400">Free
+                                            of <br> cost / Pro</h1>
+
+                                    </div>
+                                    <div class="p-5 w-1/3 bg-white rounded-md">
+                                        <h1 class="text-sm font-semibold text-center text-green-500">Free
+                                            of <br> cost / Enterprise</h1>
+
+                                    </div>
+                                </div>
+                                <!-- features & Integrations -->
+                                <div class="flex mt-3 justify-around">
+                                    <div class="">
+                                        <h1 class="text-xl font-bold">Features</h1>
+                                        <ul class="list-disc text-neutral-600 pl-5 mt-2">
+                                            <li>${
+                                              ai_features.features[1]
+                                                .feature_name
+                                            }</li>
+                                            <li>${
+                                              ai_features.features[2]
+                                                .feature_name
+                                            }</li>
+                                            <li>${
+                                              ai_features.features[3]
+                                                .feature_name
+                                            }</li>
+                                        </ul>
+                                    </div>
+                                    <div class="">
+                                        <h1 class="text-xl font-bold">Integrations</h1>
+                                        <p class="text-neutral-600 mt-2">No data Found</p>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- product div -->
+                        <div class="card card-compact border-[1px]  shadow-xl">
+                            <div class="p-3 text-center ">
+                                <div class="relative">
+                                    <img src=${
+                                      ai_features.image_link[0]
+                                    } alt="" class="rounded-md">
+                                    <p class="bg-red-600 px-2 absolute top-0 right-0 rounded-md text-white">${
+                                      ai_features.accuracy?.score * 100
+                                    } % Accuracy
+                                    </p>
+                                </div>
+                                <h1 class="text-xl font-bold mt-2">Can you give any example ?</h1>
+                                <p class="text-neutral-600 mt-2">No data Found</p>
+
+
+                            </div>
+                        </div>
+                    </div>
+  `;
   ai_details_modals.showModal();
-  console.log(ai_features);
+  console.log(ai_features.features[1]);
 };
